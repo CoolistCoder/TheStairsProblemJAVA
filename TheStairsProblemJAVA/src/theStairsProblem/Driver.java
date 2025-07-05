3c90e8d97a3f9f858933f3bf30335e5dec1459f0
Alec Roberts <22121853+CoolistCoder@users.noreply.github.com>
2025-07-05 13:56:17 -0500

    250705JAVA Updated driver to utilize inputs from user

--- TheStairsProblemJAVA/src/theStairsProblem/Driver.java
+++ TheStairsProblemJAVA/src/theStairsProblem/Driver.java
@@ -1,10 +1,20 @@
 package theStairsProblem;
 
+import java.util.Scanner;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		ClimbStairs climber = new ClimbStairs(5, 900);
+		Scanner stdin = new Scanner(System.in);
+		System.out.print("Please input a value for the number of steps in the stairs: ");
+		int numsteps = stdin.nextInt();
+		System.out.println();
+		System.out.print("Please input how many steps you can take at one time: ");
+		int numstepSize = stdin.nextInt();
+		System.out.println();
+		ClimbStairs climber = new ClimbStairs(numsteps, numstepSize);
 		climber.print();
+		stdin.close();
 	}
 
 }
