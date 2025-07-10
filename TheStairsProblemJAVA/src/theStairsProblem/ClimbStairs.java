8bcba4f3967a1bc2c441b4460d10d0aba85246ff
Alec Roberts <22121853+CoolistCoder@users.noreply.github.com>
2025-07-09 20:16:08 -0500

    250709JAVA Enhanced comments in the object to include descriptions

--- TheStairsProblemJAVA/src/theStairsProblem/ClimbStairs.java
+++ TheStairsProblemJAVA/src/theStairsProblem/ClimbStairs.java
@@ -1,20 +1,30 @@
 package theStairsProblem;
 
 public class ClimbStairs {
+	//private class vars for number of steps in stair and step size
 	private int steps, stepsize;
 	
+	//setter for steps
 	public void setSteps(int val) {
 		this.steps = val;
 	}
 	
+	//setter for step size
 	public void setStepSize(int val) {
 		this.stepsize = val;
 	}
 	
+	//method to run the steps solver
 	public int climb() {
+		//set up array with the steps in the stair
 		int[] sz = new int[this.steps+1];
+		
+		//min answer will always be one step
 		sz[0] = 1;
 		
+		//go through each step and compare to next
+		//if equal or greater than the next, increase
+		//this step by next step size
 		for (int i=1; i<=this.steps; ++i) {
 			sz[i]=0;
 			for (int j=0; j<=this.stepsize; ++j) {
@@ -24,6 +34,7 @@ public class ClimbStairs {
 			}
 		}
 		
+		//return the result
 		int result = sz[this.steps];
 		return result;
 	}
