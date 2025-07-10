ea0fac90b602bd5b88cb69713fb2f4f4c79e9ad0
Alec Roberts <22121853+CoolistCoder@users.noreply.github.com>
2025-07-09 20:10:41 -0500

    250709JAVA Enhanced comments in driver

--- TheStairsProblemJAVA/src/theStairsProblem/Driver.java
+++ TheStairsProblemJAVA/src/theStairsProblem/Driver.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
+		//create stdin for user input, then prompt
 		Scanner stdin = new Scanner(System.in);
 		System.out.print("Please input a value for the number of steps in the stairs: ");
+		
+		//retrieve user input for the number of steps in the stairs
 		int numsteps = stdin.nextInt();
 		System.out.println();
+		
+		//prompt again, then take user input for step size
 		System.out.print("Please input how many steps you can take at one time: ");
 		int numstepSize = stdin.nextInt();
 		System.out.println();
+		
+		//create the ClimbStairs object, run the print function
 		ClimbStairs climber = new ClimbStairs(numsteps, numstepSize);
 		climber.print();
 		stdin.close();
